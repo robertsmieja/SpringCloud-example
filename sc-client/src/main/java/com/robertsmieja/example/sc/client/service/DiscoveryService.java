262c2cfccc3ddfaeb6d54bffd3cb188deedb37c9
Robert Smieja <robertsmieja@gmail.com>
2018-04-11 00:11:15 -0400

    Update application.properties

--- sc-client/src/main/java/com/robertsmieja/example/sc/client/service/DiscoveryService.java
+++ sc-client/src/main/java/com/robertsmieja/example/sc/client/service/DiscoveryService.java
@@ -1,6 +1,10 @@
 package com.robertsmieja.example.sc.client.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.shell.standard.ShellComponent;
@@ -9,12 +13,17 @@ import org.springframework.shell.standard.ShellOption;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 @ShellComponent
 public class DiscoveryService {
+    @Value("${spring.application.name}")
+    static String applicationName;
+
     DiscoveryClient discoveryClient;
+    ObjectMapper objectMapper;
 
     @ShellMethod("Get all Eureka Services")
     List<String> getServices() {
@@ -22,7 +31,14 @@ public class DiscoveryService {
     }
 
     @ShellMethod("Get all Eureka instances by ID")
-    List<ServiceInstance> getInstancesById(@ShellOption("${spring.application.name}") String id) {
-        return discoveryClient.getInstances(id);
+    List<String> getInstancesById(@ShellOption(defaultValue = "sc-client") String id) {
+        return discoveryClient.getInstances(id).stream()
+                .map(this::serviceInstanceToString)
+                .collect(Collectors.toList());
+    }
+
+    @SneakyThrows(JsonProcessingException.class)
+    private String serviceInstanceToString(ServiceInstance serviceInstance){
+        return "ServiceInstance=" + objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(serviceInstance);
     }
 }
